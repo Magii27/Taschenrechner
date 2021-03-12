b811907711a1fd66ccee65894eef2456f477512e
Magii27 <80420368+Magii27@users.noreply.github.com>
2021-03-12 15:54:13 +0100

    Add files via upload

--- binär.py
+++ binär.py
@@ -2,25 +2,29 @@ def binär(eingabe):
     eingabestr = str(eingabe)
     eingabeint = int(eingabe)
     count = 0
+    ausgabe = 0
     for x in eingabestr:
         print(x)
-        if eingabe[count:count] == 1 or eingabe[count:count] == 0:
+        if x == "1" or x == "0":
             var_prüf = 0
             count += 1
         else:
             var_prüf = 1
             break
-
-    var_erg = 0
+    count = 1
     if var_prüf == 0:
         for x in eingabestr:
-            var_zw = int(eingabeint[x:x] * (2 ** x))
+            var_zw = int(x) * (2 ** (len(eingabestr) - count))
             ausgabe = int(ausgabe + var_zw)
+            count = count + 1
     else:
+        while eingabeint == 0:
+            eingabeint
+
         print("moin")
         ausgabe = 2
-        return ausgabe;
+    return ausgabe;
 
 
-var = str(input())
+var = input()
 print(binär(var))
